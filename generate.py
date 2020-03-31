a6dde316b128012d665a6a8f373624c561017b6b
amollberg <amollberg@users.noreply.github.com>
2020-03-31 22:00:47 +0200

    Snap to 45-degree angles

--- generate.py
+++ generate.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 from tkinter import *
+from collections import namedtuple
+import math
+
+class Vec(namedtuple('Vec', 'x y')):
+  pass
+
+def fold(vec, fold_matrix):
+  x, y = vec
+  (fold_xx, fold_xy), (fold_yx, fold_yy) = fold_matrix
+  return Vec(x * fold_xx + y * fold_yx,
+             x * fold_xy + y * fold_yy)
+
+def invert(matrix2x2):
+  (a, b), (c, d) = matrix2x2
+  det = 1/(a*d - b*c)
+  return ((det * d, -det * b), (-det * c, det * a))
+
+def unfold(vec, fold_matrix):
+  return fold(vec, invert(fold_matrix))
 
 class Line():
   def __init__(self):
@@ -11,6 +30,48 @@ class Line():
 
   def coords(self):
     return (self.start_x, self.start_y, self.end_x, self.end_y)
+
+  def snap_to_45(self):
+    """ Make the end the nearest point that also makes the line
+        slope a multiple of 45 degrees
+    """
+    vec = Vec(self.end_x - self.start_x,
+              self.end_y - self.start_y)
+    foldings = []
+    if vec.x < 0:
+      foldings.append(((-1, 0),(0, 1)))
+      vec = fold(vec, foldings[-1])
+    if vec.y < 0:
+      foldings.append(((1, 0),(0, -1)))
+      vec = fold(vec, foldings[-1])
+    if vec.x < vec.y:
+      foldings.append(((0, 1), (1, 0)))
+      vec = fold(vec, foldings[-1])
+    # Perform snapping
+    if vec.y/vec.x < math.tan(45/2 * math.pi/180):
+      # Project to x-axis
+      vec = Vec(vec.x, 0)
+    else:
+      # Project to diagonal
+      c = (vec.x + vec.y)/2
+      vec = Vec(c, c)
+
+    # Undo folding
+    for folding in reversed(foldings):
+      vec = unfold(vec, folding)
+    self.end_x = self.start_x + vec.x
+    self.end_y = self.start_y + vec.y
+
+def test_line():
+  line = Line()
+  line.start_x = 0
+  line.start_y = 0
+  line.end_x = 3
+  line.end_y = 100
+  line.snap_to_45()
+  assert(line.end_x == 0)
+  assert(line.end_y == 100)
+test_line()
 
 class Window(Frame):
   def __init__(self, master=None):
@@ -38,6 +99,7 @@ def main():
   def drag(event):
     dragline_coords.end_x = event.x
     dragline_coords.end_y = event.y
+    dragline_coords.snap_to_45()
     draw()
 
   canvas.bind("<Button-1>", start_drag)
