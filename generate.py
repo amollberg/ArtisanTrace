50aeea680e400cc4ced328795c2ef24f8c05524f
amollberg <amollberg@users.noreply.github.com>
2020-03-31 21:15:43 +0200

    Draw line of start to end of drag

--- generate.py
+++ generate.py
@@ -2,6 +2,16 @@
 
 from tkinter import *
 
+class Line():
+  def __init__(self):
+    self.start_x = -1
+    self.start_y = -1
+    self.end_x = -1
+    self.end_y = -1
+
+  def coords(self):
+    return (self.start_x, self.start_y, self.end_x, self.end_y)
+
 class Window(Frame):
   def __init__(self, master=None):
     Frame.__init__(self, master)
@@ -14,16 +24,24 @@ def main():
   root.wm_title("Tkinter window test")
 
   canvas = Canvas(root, bg="black", height=600, width=600)
+  dragline = canvas.create_line((-1, -1, -1, -1), fill='red', width=3)
 
-  def xy(event):
-    global lastx, lasty
-    lastx, lasty = event.x, event.y
-  canvas.bind("<Button-1>", xy)
+  dragline_coords = Line()
 
-  def addLine(event):
-    canvas.create_line(lastx, lasty, event.x, event.y, fill='red', width=3)
-    xy(event)
-  canvas.bind("<B1-Motion>", addLine)
+  def draw():
+    canvas.coords(dragline, dragline_coords.coords())
+
+  def start_drag(event):
+    dragline_coords.start_x = event.x
+    dragline_coords.start_y = event.y
+
+  def drag(event):
+    dragline_coords.end_x = event.x
+    dragline_coords.end_y = event.y
+    draw()
+
+  canvas.bind("<Button-1>", start_drag)
+  canvas.bind("<B1-Motion>", drag)
 
   canvas.pack()
   root.mainloop()
