cd97c5e71a5c74952e58668ebf9d644dedaf31a6
amollberg <amollberg@users.noreply.github.com>
2020-05-23 10:45:38 +0200

    Create Trace with draw tool and viewmodel

--- openrndr/src/main/kotlin/Main.kt
+++ openrndr/src/main/kotlin/Main.kt
@@ -5,21 +5,49 @@ import org.openrndr.math.Vector2
 import org.openrndr.shape.SegmentJoin.MITER
 import org.openrndr.shape.contours
 
-fun offsetPoly(drawer: Drawer, points: List<Vector2>, mousePoint: Vector2) {
-    val allPoints = points + mousePoint
-    val cs = contours {
-        allPoints.forEachIndexed { i, point ->
-            if (i == 0) moveTo(point)
-            else lineTo(point)
-        }
-    }
-    if (cs.isNotEmpty()) {
-        val c = cs.first()
-        drawer.contour(c)
-        drawer.contour(c.offset(15.0, joinType = MITER))
-        drawer.contour(c.offset(-15.0, joinType = MITER))
+class Trace(var points: MutableList<Vector2> = mutableListOf<Vector2>()) {
+
+    constructor(points: Iterable<Vector2>) : this(points.toMutableList())
+
+    fun withPoint(point : Vector2): Trace {
+        return Trace(points + point)
     }
 
+    fun draw(drawer: Drawer) {
+        val cs = contours {
+            points.forEachIndexed { i, point ->
+                if (i == 0) moveTo(point)
+                else lineTo(point)
+            }
+        }
+        if (cs.isNotEmpty()) {
+            val c = cs.first()
+            drawer.contour(c)
+            drawer.contour(c.offset(15.0, joinType = MITER))
+            drawer.contour(c.offset(-15.0, joinType = MITER))
+        }
+    }
+}
+
+class TraceDrawTool(var viewModel: ViewModel) {
+    private val trace = Trace()
+
+    fun mouseClicked(position : Vector2) {
+        trace.points.add(position)
+    }
+
+    fun draw(drawer : Drawer) {
+        trace.withPoint(viewModel.mousePoint).draw(drawer)
+    }
+
+    fun exit() {
+        viewModel.traces.add(trace)
+    }
+}
+
+class ViewModel {
+    var mousePoint = Vector2(-1.0, -1.0)
+    var traces : MutableList<Trace> = mutableListOf()
 }
 
 fun main() = application {
@@ -29,20 +57,20 @@ fun main() = application {
     }
 
     program {
-        var points = mutableListOf<Vector2>()
-        var mousePoint = Vector2(-1.0, -1.0)
+        var viewModel = ViewModel()
+        val traceDrawTool = TraceDrawTool(viewModel)
 
         mouse.moved.listen {
-            mousePoint = it.position
+            viewModel.mousePoint = it.position
         }
         mouse.clicked.listen {
-            points.add(it.position)
+            traceDrawTool.mouseClicked(it.position)
         }
         extend {
             drawer.fill = ColorRGBa.WHITE
             drawer.stroke = ColorRGBa.PINK
             drawer.strokeWeight = 2.0
-            offsetPoly(drawer, points, mousePoint)
+            traceDrawTool.draw(drawer)
         }
     }
 }
