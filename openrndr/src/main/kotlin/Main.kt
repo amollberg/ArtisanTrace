c6748b7b0d568907eb675454d71214eb1dc2db2d
amollberg <amollberg@users.noreply.github.com>
2020-05-24 12:20:51 +0200

    Add MouseHoverSelection

--- openrndr/src/main/kotlin/Main.kt
+++ openrndr/src/main/kotlin/Main.kt
@@ -3,12 +3,39 @@ import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+
+abstract class BaseSelection(var viewModel: ViewModel) {
+
+    open fun getInterface(): Interface? = null
+
+    open fun draw(drawer: Drawer) {}
+}
+
+class MouseHoverSelection(viewModel: ViewModel) : BaseSelection(viewModel) {
+    private val selectedInterface: Interface? = null
+
+    override fun getInterface(): Interface? {
+        // Get the interface nearest to the mouse
+        return viewModel.interfaces.minBy {
+            (it.center - viewModel.mousePoint).length
+        }
+    }
+
+    override fun draw(drawer: Drawer) {
+        val selectedInterface = getInterface()
+        if (selectedInterface != null) {
+            drawer.circle(Circle(selectedInterface.center, 8.0))
+        }
+    }
+}
 
 class ViewModel {
     var interfaces: MutableList<Interface> = mutableListOf()
     var mousePoint = Vector2(-1.0, -1.0)
     var traces : MutableList<Trace> = mutableListOf()
     var activeTool : BaseTool = EmptyTool(this)
+    var activeSelection : BaseSelection = MouseHoverSelection(this)
 
     fun keyUp(key : KeyEvent) {
         when (key.name) {
@@ -21,6 +48,7 @@ class ViewModel {
     fun draw(drawer: Drawer) {
         traces.forEach { it -> it.draw(drawer) }
         interfaces.forEach { it -> it.draw(drawer) }
+        activeSelection.draw(drawer)
         activeTool.draw(drawer)
     }
 
