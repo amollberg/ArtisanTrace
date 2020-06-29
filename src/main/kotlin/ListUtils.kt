a71737badacd89aad227714c6c7b17a6c22f2f43
amollberg <amollberg@users.noreply.github.com>
2020-06-29 20:41:57 +0200

    Add crossProduct list utility

--- src/main/kotlin/ListUtils.kt
+++ src/main/kotlin/ListUtils.kt
@@ -2,3 +2,9 @@
 fun <T> rotateFront(l: List<T>): List<T> =
     if (l.size == 1) l
     else l.drop(1) + l[0]
+
+// Return all combinations of elements from a and from b
+fun <T> crossProduct(a: Iterable<T>, b: Iterable<T>) =
+    a.flatMap { ae ->
+        b.map { be -> Pair<T, T>(ae, be) }
+    }
--- src/test/kotlin/ListUtilsTest.kt
+++ src/test/kotlin/ListUtilsTest.kt
@@ -7,4 +7,24 @@ class ListUtilsTest {
         val elements = listOf(0, 1, 2, 3, 4)
         assertEquals(listOf(1, 2, 3, 4, 0), rotateFront(elements))
     }
+
+    @Test
+    fun testCrossProduct() {
+        val a = listOf(10, 20, 30)
+        val b = listOf(0, 1, 2)
+        assertEquals(
+            listOf(
+                Pair(10, 0),
+                Pair(10, 1),
+                Pair(10, 2),
+                Pair(20, 0),
+                Pair(20, 1),
+                Pair(20, 2),
+                Pair(30, 0),
+                Pair(30, 1),
+                Pair(30, 2)
+            ),
+            crossProduct(a, b)
+        )
+    }
 }
