cb5feade95a8bbf75f58366fd944ba68c8716f47
amollberg <amollberg@users.noreply.github.com>
2020-06-29 21:17:20 +0200

    Add start, end properties to seg ptr

--- src/main/kotlin/SegmentPointer.kt
+++ src/main/kotlin/SegmentPointer.kt
@@ -2,8 +2,9 @@ import coordinates.System
 import org.openrndr.shape.Segment
 
 data class SegmentPointer(val poly: Poly, val index: Int) {
-    fun segment(system: System): Segment = Segment(
-        poly.points[index].xyIn(system),
-        poly.points[Math.floorMod(index + 1, poly.points.size)].xyIn(system)
-    )
+    fun segment(system: System): Segment =
+        Segment(start.xyIn(system), end.xyIn(system))
+
+    val start get() = poly.points[index]
+    val end get() = poly.points[Math.floorMod(index + 1, poly.points.size)]
 }
