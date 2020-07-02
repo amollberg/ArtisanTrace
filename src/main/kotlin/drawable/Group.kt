5aa8090b1c7a013ff59618cbd713ae13177733a8
amollberg <amollberg@users.noreply.github.com>
2020-07-02 21:05:22 +0200

    Construct and visualize group surface from sorted members

--- src/main/kotlin/drawable/Group.kt
+++ src/main/kotlin/drawable/Group.kt
@@ -1,4 +1,6 @@
 import kotlinx.serialization.Serializable
+import org.openrndr.color.ColorRGBa.Companion.TRANSPARENT
+import org.openrndr.color.ColorRGBa.Companion.YELLOW
 
 @Serializable
 data class Group(
@@ -12,7 +14,14 @@ data class Group(
     val members: Set<GroupMember>
         get() = interfaces + traces + sketchComponents + svgComponents
 
+    val surface: Poly
+        get() = members.sortedBy { it.groupOrdinal }.map { it.bounds }
+            .fold(Poly(emptyList())) { a, b ->
+                Poly.fuse(a, b)
+            }
+
     fun add(groupMember: GroupMember) {
+        groupMember.groupOrdinal = members.size
         when (groupMember::class) {
             Interface::class -> interfaces.add(groupMember as Interface)
             Trace::class -> traces.add(groupMember as Trace)
@@ -39,5 +48,13 @@ data class Group(
         traces.forEach { it.draw(drawer) }
         sketchComponents.forEach { it.draw(drawer) }
         svgComponents.forEach { it.draw(drawer) }
+        isolatedStyle(
+            drawer.drawer,
+            stroke = YELLOW,
+            fill = TRANSPARENT
+        ) {
+            if (drawer.extendedVisualization)
+                surface.draw(drawer)
+        }
     }
 }
