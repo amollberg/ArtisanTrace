1ec3b403813de9487835b411a205cb04ac32b7d9
amollberg <amollberg@users.noreply.github.com>
2020-07-09 20:59:47 +0200

    Register Via2 interface positions, prep for compensation

---
+++ src/test/kotlin/ResourceTest.kt
@@ -0,0 +1,31 @@
+import TestUtils.Companion.assertEquals
+import TestUtils.Companion.dropFiles
+import org.junit.jupiter.api.Test
+import org.openrndr.DropEvent
+import org.openrndr.math.Vector2
+import java.io.File
+
+class ResourceTest : WithImplicitView() {
+    @Test
+    fun ic1ExpectedInterfaceEnds() {
+        checkResource("src/test/resources/IC1.svg")
+    }
+
+    @Test
+    fun via2ExpectedInterfaceEnds() {
+        checkResource("src/test/resources/Via2.svg")
+    }
+
+    private fun checkResource(path: String) {
+        dropFiles(
+            view,
+            DropEvent(Vector2.ZERO, listOf(File(path)))
+        )
+        assertEquals(
+            EXPECTED_INTERFACE_ENDS[path]!!,
+            view.model.svgComponents.first().interfaces.map {
+                it.getEnds().map { it.xyIn(view.root) }
+            }, delta = 1e-5
+        )
+    }
+}
--- src/test/kotlin/Resources.kt
+++ src/test/kotlin/Resources.kt
@@ -6,6 +6,9 @@ val EXPECTED_INTERFACE_ENDS = hashMapOf(
     "src/test/resources/IC1.svg" to listOf(
         listOf(Vector2(8.0, 10.0), Vector2(8.0, 25.0)),
         listOf(Vector2(22.0, 10.0), Vector2(22.0, 25.0))
+    ),
+    "src/test/resources/Via2.svg" to listOf(
+        listOf(Vector2(0.63883413, 10.956321), Vector2(-0.63883413, 10.956321))
     )
 )
 
