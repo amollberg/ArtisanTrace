5744e197cba1d6d99eedb9ea175e16a0ba5d8ab2
amollberg <amollberg@users.noreply.github.com>
2020-07-15 23:01:49 +0200

    Generate and add SVG on ATG file drop

--- src/main/kotlin/drawable/ViewModel.kt
+++ src/main/kotlin/drawable/ViewModel.kt
@@ -11,6 +11,7 @@ import org.openrndr.draw.Drawer
 import org.openrndr.events.Event
 import org.openrndr.math.Vector2
 import org.openrndr.shape.CompositionDrawer
+import org.openrndr.svg.writeSVG
 import tool.ColorPickTool
 import java.io.File
 import java.io.FileNotFoundException
@@ -137,6 +138,11 @@ class ViewModel(internal var model: Model) {
             when (droppedFile.extension) {
                 "svg" ->
                     handleDroppedSvgFile(droppedFile, root.coord(drop.position))
+                "atg" ->
+                    handleDroppedSvgMacroFile(
+                        droppedFile,
+                        root.coord(drop.position)
+                    )
                 else ->
                     // Treat as a sketch file containing a model
                     handleDroppedSketchFile(
@@ -144,6 +150,27 @@ class ViewModel(internal var model: Model) {
                         root.coord(drop.position)
                     )
             }
+        }
+    }
+
+    private fun handleDroppedSvgMacroFile(
+        droppedFile: File,
+        coord: Coordinate
+    ) {
+        val content = droppedFile.readText()
+        val obj = maybeMuteExceptions {
+            SvgMacro.json.parse(SvgMacro.serializer(), content)
+        }
+        obj?.ifPresent {
+            val svgFile = File(droppedFile.absoluteFile.path + ".svg")
+            val cd = CompositionDrawer()
+            when (obj) {
+                is SvgMacro.RectGrid -> obj.draw(cd)
+                is SvgMacro.VerticalPins -> obj.draw(cd)
+            }
+            val svgText = writeSVG(cd.composition)
+            svgFile.writeText(svgText)
+            model.addSvg(svgFile, coord)
         }
     }
 
