ee6f6ebddca106e6ef8b0e58c280c3f83529a2f9
amollberg <amollberg@users.noreply.github.com>
2020-07-29 11:59:54 +0200

    Extract SnappingUtils for reuse

--- src/main/kotlin/tool/InterfaceMoveTool.kt
+++ src/main/kotlin/tool/InterfaceMoveTool.kt
@@ -95,38 +95,3 @@ class InterfaceMoveTool(viewModel: ViewModel) : BaseTool(viewModel) {
         }
     }
 }
-
-// Returns the Length to move the movingPoly so that it borders the poly
-// while trying to approach the targetPosition
-fun snappedTo(movingPoly: Poly, poly: Poly, target: Coordinate):
-        Length {
-    val originToTarget = target - movingPoly.system!!.originCoord
-    val snapSegment = nearestSegment(poly, target)
-        ?: return originToTarget
-    val snapPoint =
-        target.system.coord(
-            snapSegment.segment(target.system).project(target.xy()).point
-        )
-    // Assuming for now that the projection of target is on the snapSegment
-
-    val movingPoint = movingPoly.points.minBy { movingPoint ->
-        countOverlappingPoints(movingPoly.moved(snapPoint - movingPoint), poly)
-    } ?: return originToTarget
-
-    return snapPoint - movingPoint
-}
-
-fun countOverlappingPoints(a: Poly, b: Poly) =
-    a.points.filter { b.contains(it) }.count() +
-            b.points.filter { a.contains(it) }.count()
-
-fun nearestSegment(poly: Poly, point: Coordinate) =
-    poly.segmentPointers.minBy {
-        it.segment(point.system).project(point.xy()).distance
-    }
-
-fun offsetOutwards(poly: Poly, distance: Double): Poly {
-    return listOf(distance, -distance).map { offset ->
-        poly.offsetBounds(offset)
-    }.minBy { countOverlappingPoints(it, poly) }!!
-}
